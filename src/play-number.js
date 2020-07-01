5088d84622c9897ba064a20f0264e03c3394199e
Robert Luck <rl19aaj@herts.ac.uk>
2020-07-01 11:12:39 +0100

    Move button functionality into seperate PlayNumber component

---
+++ src/play-number.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./global.css"
+
+const PlayNumber = (props) => (
+    <button className="number" onClick={() => console.log('Num', props.number)}>
+        {props.number}
+    </button>
+)
+
+export default PlayNumber
